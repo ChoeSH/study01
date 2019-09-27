741481a3b3301a124bc0b5036fe69fedc462e6a1
Administrator <Administrator@koitt03a-PC>
2019-09-27 15:20:31 +0900

    commit만

--- src/study01/test03/HowOldRU.java
+++ src/study01/test03/HowOldRU.java
@@ -9,10 +9,25 @@ public class HowOldRU {
 		System.out.println("너 몇살이야?");
 		String age = scan.nextLine();	//String age = scan.nextLine(); age값을 입력받고 enter를 누르기전 값을 저장
 		System.out.println("난 "+age+"살 이야");
-		int num = Integer.parseInt(age);	//age값을 int형식으로 변환
-				
+		int num = Integer.parseInt(age);	//age값을 int형식으로 변환 , 에러를 염두해두고 사용해야함
+		if(Integer.parseInt(age)<=10) {
+			System.out.println("어린이");
+		}
+			else if(num<=20) {
+				System.out.println("청소년");
+			}
+			else if(num<=40) {
+				System.out.println("청년");
+			}
+			else{
+				System.out.println("꼰대");
+			}
+		}
 	}
-}
 
 //형상관리툴 : git, Svn, starteam
 //gitignore
+
+/*Scanner scan = new Scanner(System.in);
+String age = scan.nextLine();
+int num = Integer.parseint(age);*/
